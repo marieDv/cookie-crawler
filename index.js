fe7163ee4fe87dcc77ff31089c4a627bed4148f4
marie <dvorzak.marie@gmx.at>
2022-09-05 12:13:29 +0200

    check if name is already saved into database

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // var Crawler = require("crawler");
 // const Crawler = require('crawler');
 import * as fs from 'fs';
-
+import { Level } from 'level';
 import Crawler from 'crawler';
 import nlp from 'de-compromise'
 var allNames = [];
 var allURLS = [];
 var fullData = [];
-//const fs = require('fs');
+var fullCounter = 0;
+const db = new Level('namesLevel', { valueEncoding: 'json' })
+const dbUrl = new Level('urlsLevel', { valueEncoding: 'json' })
 
+//const fs = require('fs');
 // const Crawler = require('crawler');
 // var fs = require("fs");
 
 
 var completeData = [];
-
-
-
 let obselete = []; // Array of what was crawled already
 let c = new Crawler({
   maxConnections: 10,
@@ -24,8 +24,14 @@ let c = new Crawler({
   // encoding: null,
   // jQuery: false,
 });
-crawlAllUrls('https://www.schoenbrunn.at/');//https://www.ait.ac.at/en/
 
+init();
+
+
+function init() {
+  db.clear();
+  crawlAllUrls('https://futuress.org/learning/coding-resistance/');//https://www.ait.ac.at/en/
+}
 
 function crawlAllUrls(url) {
   // console.log(`Crawling ${url}`);
@@ -46,14 +52,19 @@ function crawlAllUrls(url) {
               setTimeout(function () {
                 href.startsWith('http') ? crawlAllUrls(href) : crawlAllUrls(`${url}${href}`)
                 // href.startsWith(url) ? crawlAllUrls(href) : crawlAllUrls(`${url}${href}`)
-                // console.log($("p").text());
-                // let data = JSON.stringify($("body").text());
                 let data = $("body").text();
-                // console.log($("body").text());
                 let pageTitle = JSON.stringify(`${url}`);
-                // console.log(urls[item].attribs.href);
                 completeData.push('[[' + pageTitle + '],[' + data + ']]');
-                searchForNames(urls[item].attribs.href, data);
+                if (data) {
+                  dbUrl.get(url, function (err, key) {
+                    if (err) {
+                      console.log("new url " + url);
+                      searchForNames(urls[item].attribs.href, data);
+                    } else {
+                      // console.log("revisited url: " + key);
+                    }
+                  })
+                }
               }, 1000)
 
             }
@@ -73,32 +84,54 @@ function crawlAllUrls(url) {
 function searchForNames(url, data) {
 
   let doc = nlp(data);
-  let number = doc.match('#Person #Noun').out('array');
-  console.log(number);
-  let person = doc.match('#Person #Noun').out('array')
+  let person = doc.match('#Person #Noun')
   person = person.forEach(function (d, i) {
-    // console.log(d.text('reduced'));
     allURLS[i] += `url': `;
     allURLS[i] += "'" + url;
 
-    allNames[i] += `name': `;
-    allNames[i] += "'" + d.text('reduced');
+
+
+
+    db.get(d.text('reduced'), function (err, key) {
+      if (err) {
+        allNames[fullCounter] = `name': `;
+        allNames[fullCounter] += "'" + d.text('reduced');
+        fullCounter++;
+        if (allNames[fullCounter] !== null) {
+          writeToJsonFile(d.text('reduced'));
+        }
+
+      } else {
+        // console.log("already there");
+      }
+    })
+
+    db.put(d.text('reduced'), d.text('reduced'));
+    dbUrl.put(url, url);
     fullData[i, 0] = allURLS[i];
     fullData[i, 1] = allNames[i];
-    // allNames[i] = `'name': '`+d.text('reduced')+`'`;
+
 
   })
   doc.text()
-  // console.log(allNames);
-  // console.log(JSON.stringify(allNames));
 
-  setTimeout(() => {
-    const file = fs.readFileSync('names.json');
-    // fs.writeFileSync('names.json', JSON.stringify(allNames));
-    var json = JSON.parse(file.toString());
-    // json.push(allURLS);
-    json.push(fullData);
-    fs.writeFileSync("names.json", JSON.stringify(json))
-  }, 1000);
+
+  // setTimeout(() => {
+  // console.log(allNames);
+  // }, 1000);
+
+}
+
+
+
+
+///// **** HELPER
+
+
+function writeToJsonFile(mData) {
+  const file = fs.readFileSync('names.json');
+  var json = JSON.parse(file.toString());
+  json.push(mData);
+  fs.writeFileSync("names.json", JSON.stringify(json))
 
 }
