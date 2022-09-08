29ef1c336682e5b3637931f6ac6a71a52d34cbaf
Marie Dvorzak <dvorzak.marie@gmx.at>
2022-09-08 10:53:06 +0200

    test

--- functions.js
+++ functions.js
@@ -59,7 +59,7 @@ export function readJsonFile() {
 export function writeLatestToTerminal(mydata) {
   const file = fs.readFileSync('names.json');
   var mydata = JSON.parse(file.toString());
-  term.fullscreen(true);
+  // term.fullscreen(true);
   term.table([
     ['name', 'countrycode', 'date', 'language'],
     [mydata[mydata.length - 1] ? mydata[mydata.length - 1][0].name : '', mydata[mydata.length - 1] ? mydata[mydata.length - 1][0].countrycode : '', mydata[mydata.length - 1] ? mydata[mydata.length - 1][0].date : '', mydata[mydata.length - 1] ? mydata[mydata.length - 1][0].language : ''],// mydata[mydata.length - 5][0].countrycode, mydata[mydata.length - 9][0].date],
